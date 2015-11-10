a327f7e01689d5daf69e1a8d1471dc71214480e4
asabalon <aaabalon@itdc.up.edu.ph>
2015-11-10 16:35:47 +0800

    Added static files in add_appointment.html

--- appointments/models.py
+++ appointments/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Appointment(models.Model):
-	petOwnerName = models.CharField(max_length=50, blank=False, editable=False)
-	petDescription = models.CharField(max_length=100, blank=False)
+	petOwnerName = models.CharField(max_length=50, editable=False)
+	petDescription = models.CharField(max_length=100)
 	visitSchedule = models.DateTimeField(auto_now=False, auto_now_add=False)
-	visitDescription = models.CharField(max_length=100, blank=False)
-	veterinaryPhysician = models.CharField(max_length=100, blank=False)
+	visitDescription = models.CharField(max_length=100)
+	veterinaryPhysician = models.CharField(max_length=100)
 
 
 	def __str__(self):
